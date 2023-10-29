3bb407ea5b588043c0acd42166f9654734b12464
Gustavo Gonzaga <gustavosg91@gmail.com>
2023-10-28 23:00:14 -0300

    Relacionamentos entre modelos

---
+++ src/adminJS/resources/index.ts
@@ -0,0 +1,16 @@
+import { ResourceWithOptions } from "adminjs";
+import { Category } from "../../models/Category";
+import { categoryResourceOptions } from "./category";
+import { Course } from "../../models/Course";
+import { courseResourceOptions } from "./course";
+
+export const adminJsResources: ResourceWithOptions[] = [
+    {
+        resource: Category,
+        options: categoryResourceOptions
+    },
+    {
+        resource: Course,
+        options: courseResourceOptions
+    }
+]
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import express from 'express';
 import { sequelize } from './database';
+import { adminJs, adminJsRouter } from './adminJS';
 
 const app = express();
+
+app.use(express.static('public'));
+
+app.use(adminJs.options.rootPath, adminJsRouter);
 
 const PORT = process.env.PORT || 3000;
 
@@ -9,6 +14,6 @@ app.listen(PORT, () => {
     sequelize.authenticate().then(() => {
         console.log('Conectado ao banco de dados com sucesso!');
     });
-    
+
     console.log(`Servidor iniciado com sucesso na porta ${PORT}`)
 })
